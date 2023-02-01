3bbcc23b967446d224153f75e03ff33fba6ac4e0
Peter C <57115079+PCao2@users.noreply.github.com>
2023-02-01 17:26:16 -0600

    Initial Commit

---
+++ Assets/Scripts/MirrorPosition1.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MirrorPosition1 : MonoBehaviour
+{
+    public GameObject mirrorParent;
+
+    //game ball's transform
+    public GameObject mirrorChild;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 parentPosition = mirrorParent.transform.position;
+        Quaternion parentRotation = mirrorParent.transform.rotation;
+
+        mirrorChild.transform.SetPositionAndRotation(parentPosition, parentRotation);
+    }
+}
