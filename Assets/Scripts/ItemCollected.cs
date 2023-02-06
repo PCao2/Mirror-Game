74243a4df91aaeb04335fdf135bfca5a118160f4
Peter C <57115079+PCao2@users.noreply.github.com>
2023-02-06 16:25:08 -0600

    Fixed bug with collectibles

--- Assets/Scripts/ItemCollected.cs
+++ Assets/Scripts/ItemCollected.cs
@@ -1,24 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemCollected : MonoBehaviour
 {
-    public int objectsCollected;
-
     // Number of collectibles needed to unlock door
-    private int collectGoal = 5;
+    public int collectGoal = 5;
+
+    public GameObject door;
+
+    public int objectsCollected = 0;
+    private float scaleMaxHeight;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        scaleMaxHeight = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        return;
+    }
+
+    // Update the scale's position and text based on proportion of collectibles collected
+    void AdjustScale()
+    {
+        float totalMovement = 0.05f;
+        float adjustment = (objectsCollected / (float) collectGoal) * totalMovement;
+        float scaleCurrentHeight = scaleMaxHeight - adjustment;
+
+        Vector3 scaleTopPosition = transform.position;
+        Vector3 scaleTopNewPosition = new Vector3(scaleTopPosition.x, scaleCurrentHeight, scaleTopPosition.z);
+
+        transform.position = scaleTopNewPosition;
     }
 
     void UnlockDoor()
@@ -31,6 +47,7 @@ public class ItemCollected : MonoBehaviour
         if (other.gameObject.CompareTag("Collectible"))
         {
             objectsCollected = objectsCollected + 1;
+            AdjustScale();
 
             if (objectsCollected == collectGoal)
             {
@@ -44,6 +61,7 @@ public class ItemCollected : MonoBehaviour
         if (other.gameObject.CompareTag("Collectible"))
         {
             objectsCollected = objectsCollected - 1;
+            AdjustScale();
         }
     }
 }
